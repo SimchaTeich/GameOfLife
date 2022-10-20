36117d8190e19e28da239cad3256bdffd65de0f6
SimchaTeich <sim.teich@gmail.com>
2022-10-20 13:57:35 +0300

    implement class Cell

---
+++ GameOfLife/GameOfLife/Cell.cpp
@@ -0,0 +1,21 @@
+#include "Cell.h"
+
+Cell::Cell() : _isAlive(false)
+{}
+
+bool Cell::isAlive()
+{
+	return this->_isAlive;
+}
+
+void Cell::revive()
+{
+	this->_isAlive = true;
+}
+
+void Cell::kill()
+{
+	this->_isAlive = true;
+}
+
+
